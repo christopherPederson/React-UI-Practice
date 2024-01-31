37f59a6f57f391f4948a674a75ab15d78df0b95b
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-01-31 06:49:06 -0500

    updated modal

--- react-project-v3/src/components/EMPLOYEE-MODAL.js
+++ react-project-v3/src/components/EMPLOYEE-MODAL.js
@@ -20,19 +20,9 @@ function Example() {
         backdrop="static"
         keyboard={false}
       >
-        <Modal.Header closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          I will not close if you click outside me. Do not even try to press
-          escape key.
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary">Understood</Button>
-        </Modal.Footer>
+        <input type="text" placeholder="Name" />
+        <input type="text" placeholder="Role" />
+        <button onClick={handleClose}>Submit</button>
       </Modal>
     </>
   );
