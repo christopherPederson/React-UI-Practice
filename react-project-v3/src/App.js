a593d1a91b993cd6198af56d92d400adf3c122fb
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-01-30 20:38:01 -0500

    added modal and uuid

--- react-project-v3/src/App.js
+++ react-project-v3/src/App.js
@@ -1,21 +1,25 @@
 import "./App.css";
 import EMPLOYEE from "./components/EMPLOYEE.js";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import MODAL from "./components/EMPLOYEE-MODAL.js";
 
 function App() {
-    const [role, setRole] = useState();
     const [employee, setEmployees] = useState([
         {
+            id: uuidv4(),
             name: "John Doe",
             role: "Manager",
             image: "https://via.placeholder.com/150",
         },
         {
+            id: uuidv4(),
             name: "Jane Doe",
             role: "Employee",
             image: "https://via.placeholder.com/150",
         },
         {
+            id: uuidv4(),
             name: "John Smith",
             role: "Employee",
             image: "https://via.placeholder.com/150",
@@ -23,7 +27,19 @@ function App() {
     ]);
     return (
         <div className="App">
-
+            {employee.map((employee) => {
+                return (
+                    <>
+                        <EMPLOYEE
+                            name={employee.name}
+                            role={employee.role}
+                            image={employee.image}
+                            id={employee.id}
+                        />
+                        <MODAL />
+                    </>
+                );
+            })}
         </div>
     );
 }
--- react-project-v3/src/components/EMPLOYEE.js
+++ react-project-v3/src/components/EMPLOYEE.js
@@ -4,6 +4,7 @@ let EMPLOYEE = (props) => {
             <img src={props.image} alt={props.name} />
             <h1>{props.name}</h1>
             <h3>{props.role}</h3>
+            <p>{props.id}</p>
         </div>
     );
 };
