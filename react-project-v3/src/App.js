c833b9cf6370909a784e6205b3404155ba0c4314
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-02-10 19:27:47 -0500

    added routing to team path

--- react-project-v3/src/App.js
+++ react-project-v3/src/App.js
@@ -1,14 +1,19 @@
 import "./App.css";
 import EmployeesGrouped from "./components/EmployeesGrouped.js";
 import Navbar from "./components/Header.js";
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 let App = () => {
     return (
-        <Navbar>
-            <BrowserRouter><EmployeesGrouped /></BrowserRouter>
-            
-        </Navbar>
+        <div className="App">
+            <Navbar>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/team" element={<EmployeesGrouped />} />
+                    </Routes>
+                </BrowserRouter>
+            </Navbar>
+        </div>
     );
 };
 
