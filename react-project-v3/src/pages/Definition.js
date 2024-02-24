82083d36bb4acb70077d3bcc60ca597f94cb58a3
christopherPederson <127063705+christopherPederson@users.noreply.github.com>
2024-02-23 19:16:43 -0500

    integrated API into Definitions Section

--- react-project-v3/src/pages/Definition.js
+++ react-project-v3/src/pages/Definition.js
@@ -1,19 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Definition() {
+    let [searchTerm, setSearchTerm] = useState("hello");
+    let [searchResults, setSearchResults] = useState([]);
+
     useEffect(() => {
-        fetch("https://api.dictionaryapi.dev/api/v2/entries/en/word").then(
-            (response) => {
-                response.json().then((data) => {
-                    console.log(data);
-                });
-            }
-        );
+        fetch(
+            "https://api.dictionaryapi.dev/api/v2/entries/en/" + searchTerm
+        ).then((response) => {
+            response.json().then((data) => {
+                setSearchResults(data[0].meanings);
+            });
+        });
     }, []);
 
     return (
-        <div>
-            <p>check console</p>
-        </div>
+        <>
+            {searchResults.map((meaning) => {
+                return (
+                    <>
+                        <p>
+                            {meaning.partOfSpeech} : 
+                            {meaning.definitions[0].definition}
+                        </p>
+                    </>
+                );
+            })}
+        </>
     );
 }
